de53459628153360bc5981ca5b2b3487098e6cae
Hezekiah <irochezekiah@gmail.com>
2026-08-13 13:04:55 +0100

    updated Schema for user

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -4,20 +4,25 @@ import userIdGen from "../middlewares/userId.js";
 
 const userSchema = new mongoose.Schema(
     {
-        userId: {type: String, default: userIdGen, unique: true},
-        fullName: {type: String, required:true, },
-        businessName: {type: String, default: null, },
-        businessLogo: { type: String, default: null},
-        email: {type: String, required: true, unique: true,  match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"]},
-        role: {type: String, enum: ['user', 'admin', 'superAdmin'], default: 'user'},
-        password: {type: String, required: true, minlength: 8},
-        
+        userId: { type: String, default: userIdGen, unique: true, index: true, },
+
+        fullName: { type: String, required: true, trim: true, },
+
+        email: { type: String, required: true, unique: true, lowercase: true, trim: true, match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"], },
+
+        password: { type: String, required: true, minlength: 8, },
+
+        role: { type: String, enum: ["user", "admin", "superAdmin"], default: "user", },
 
         isVerified: { type: Boolean, default: false, },
-        resetPasswordToken: { type: String },
-        resetPasswordExpires: { type: Date },
 
-    }, {timestamps: true}   
+        resetPasswordToken: { type: String, default: null, },
+
+        resetPasswordExpires: { type: Date, default: null, },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 userSchema.pre("save", async function () {
